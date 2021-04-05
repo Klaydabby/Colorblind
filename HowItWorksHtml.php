20ee5cfd9448845993603d0a4bbc11d5be991051
shalushivhare <78044889+shalushivhare@users.noreply.github.com>
2021-04-05 15:16:07 -0500

    Add files via upload
    I have created an html for How it works page. Also I have linked the get started button to the homepage to help the user get started with the test. 
    HowItWorksHtml.php has already been uploaded in hostinger as well.

---
+++ HowItWorksHtml.php
@@ -0,0 +1,43 @@
+<?php require_once 'nav.php';?>  
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Welcome to the TAMUC Online Colorblindness Test</title>
+    <link rel="stylesheet" href="main.css" type="text/css"/>
+  </head>
+  <body>
+      
+      <form class="form-horizontal" action='' method="POST" id="How It Works">
+            <fieldset>
+
+            <div id="legend">
+                <legend  align="left" class="fieldsetname"><b> How It Works </b></legend>
+            </div>
+
+            <div class="How It Works">
+                    <p>Getting yourself colorblind tested is as easy as 1-2-3 <button class="btn btn-warning"><a href="http://aakhatest.online/index.php"> Get Started</a></button></p>
+					<p><h6>We help you and your family know if you are a colorblind where thousands of people are unaware of it. And yes it can be done right from your home.</h6></p>
+					<ul>
+					    <ol type = "1">
+					<li> <u>Take a test</u> </li>
+					<ul>
+                    <li>Create an Account or log on to an existing one.</li>
+                    <li>Go to the homepage and click the button Red/Green Test.</li>
+                    <li>You must answer all 10 questions to get the results instantly.</li>
+                    <li>Once you have the results, you can also take the Blue/Yellow Test and follow the steps accordingly.</li>
+                    </ul>
+					<li> <u>Select the product</u></li>
+                    <p>For the best experience, we have selected the highest quality items. As a result, you can choose your item by clicking on the provided choices.</p>
+					<li> <u>Shipping</u></li>
+                    <p>After selecting your items you can proceed to checkout. Then, you make a payment using one of our many payment options.</p>
+                    </ol>
+				    <p><a "class="push" href="#">Frequently Asked Questions(FAQ)</a></p>
+			</div>
+            </fieldset>
+      </form>
+  </body>
+</html>
