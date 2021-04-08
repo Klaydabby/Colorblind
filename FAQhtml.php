4e500ff28c43622d315fa473fde7f0da415cb573
shalushivhare <78044889+shalushivhare@users.noreply.github.com>
2021-04-08 14:45:19 -0500

    Add files via upload
    FAQ page that connects to How It Works page

---
+++ FAQhtml.php
@@ -0,0 +1,41 @@
+<?php require_once 'nav.php';?>  
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>General FAQs</title>
+    <link rel="stylesheet" href="main.css" type="text/css"/>
+  </head>
+  <body>
+      
+      
+       <form class="form-horizontal" action='' method="POST" id="How It Works">
+            <fieldset>
+            <div id="legend">
+                <legend  align="left" class="fieldsetname"><b> General FAQs </b></legend>
+            </div>
+
+            <div class="FAQ">
+					<ul style="list-style-type:none">
+					<li><h4><u>Buying/Orders/Shipping</u></h4></li>
+                    <p><ul>Are there any discounts or promotions available?</br>
+                    Where do you ship?</br>
+                    Where can I find frame measurements?</ul></p>
+                
+					<li><h4><u>Prescription/Rx</u></h4></li>
+                    <p><ul>I wear prescription glasses or contacts. What should I buy?</ul></p>
+                    
+					<li> <h4><u>Post-Purchase</u></h4></li>
+                    <p><ul>What is the status of my order?</br>When will I receive my order?</br>How do I get my frames adjusted?</ul></p>
+                    </ul>
+			</div>
+			
+            </fieldset>
+      </form>
+  </body>
+</html>
+
+      
